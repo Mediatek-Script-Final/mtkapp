cc413eb31d63a4a54fcab46e044e7dea8a6bb788
Dexter Eskalarte <167977652+Mediatek-Script-Final@users.noreply.github.com>
2024-09-17 22:22:22 +0800

    Add files via upload

---
+++ src/contentData/content.js
@@ -0,0 +1,47 @@
+const productData = [
+    {
+      id: 1,
+      name: "STARLINK VPN",
+      image:
+        "https://i.ibb.co/NFWPLbQ/app-main-logo.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY"],
+      rating: 5,
+      timeLeft: 27,
+      appLink: "https://tinyurl.com/StarLinkVPNnew-Apk"
+    },
+    {
+      id: 2,
+      name: "STARLINK PRO",
+      image:
+        "https://i.postimg.cc/yYBPPWjn/shield-3408433.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+      rating: 5,
+      timeLeft: 24,
+      appLink: "https://tinyurl.com/StarLinkProNew-Apk"
+    },
+    {
+      id: 3,
+      name: "STARLINK VPN LOGIN",
+      image:
+        "https://i.ibb.co/NFWPLbQ/app-main-logo.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY"],
+      rating: 5,
+      timeLeft: 27,
+      appLink: "https://play.google.com/store/apps/details?id=app.fixbuild.StarlinkVPNv2"
+    }
+    // {
+    //   id: 4,
+    //   name: "SECURE TUNNEL",
+    //   image:
+    //     "https://i.postimg.cc/wMW7LWFC/keyhole-958517.png",
+    //   productType: "openBackHeadphones",
+    //   description: ["OVPN", "UDP", "V2RAY"],
+    //   rating: 5,
+    //   timeLeft: 24,
+    //   appLink: "https://tinyurl.com/StarLinkVPNnew-Apk"
+    // }
+  ];
+  export default productData;
---
+++ src/contentData/siteOption.js
@@ -0,0 +1,11 @@
+const siteOptionData = [
+    {
+      id: 1,
+      siteName: "STARLINK BRAND",
+      image:
+        "https://i.ibb.co/NFWPLbQ/app-main-logo.png",
+      description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
+      siteLink: "https://starlinkbrand.xyz/login"
+    }
+  ];
+  export default siteOptionData;
