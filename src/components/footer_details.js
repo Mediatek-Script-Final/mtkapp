f39d1dedb71bb6b4fbd34e50e8014b5f5aabdad9
Dexter Eskalarte <167977652+Mediatek-Script-Final@users.noreply.github.com>
2024-09-17 22:21:48 +0800

    Add files via upload

---
+++ src/components/footer_details.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FooterDetails = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <h2 className="footer-title">STARLINK VPN</h2>
+        <p className="footer-copy">&copy; 2024 FixBuild. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterDetails;
